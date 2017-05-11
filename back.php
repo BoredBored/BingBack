0530f3807619000bd575a20f9bfa93e2343c2f68
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-11 09:50:03 -0400

    Update back.php

--- back.php
+++ back.php
@@ -2,6 +2,10 @@
 /*g_img.url*/
 
 $bingHomepage = file_get_contents('http://www.bing.com/');
+$gimgString = "g_img={url: \""
+$gimgStringLength = strlen($gimgString); //im lazy
+$gimgPos = strpos($bingHomepage, $gimgString);
+$bingBackImageURL = $gimgPos + $gimgStringLength;
 
 /*
 files = glob.glob('%s/Downloads/*.xls'%Home)
