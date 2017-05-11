da31bf802d7d1e348f0e1d6cb7261885b6288901
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-11 09:22:31 -0400

    Update and rename back.py to back.php

---
+++ back.php
@@ -0,0 +1,10 @@
+/*g_img.url*/
+
+
+
+/*
+files = glob.glob('%s/Downloads/*.xls'%Home)
+
+newest = min(files, key=os.path.getctime)
+os.system("gsettings get org.gnome.desktop.background picture-uri 'file:///%s/Downloads/%s'"% (Home, newest))*/
+
