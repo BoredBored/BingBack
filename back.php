469e3b279282b5a3141c8dc9ea2b213368bd1c32
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-11 09:26:27 -0400

    Update back.php

--- back.php
+++ back.php
@@ -1,6 +1,7 @@
+<?php
 /*g_img.url*/
 
-
+$bingHomepage = file_get_contents('http://www.bing.com/');
 
 /*
 files = glob.glob('%s/Downloads/*.xls'%Home)
@@ -8,3 +9,4 @@ files = glob.glob('%s/Downloads/*.xls'%Home)
 newest = min(files, key=os.path.getctime)
 os.system("gsettings get org.gnome.desktop.background picture-uri 'file:///%s/Downloads/%s'"% (Home, newest))*/
 
+?>
