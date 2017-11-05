9ea61a361c0b0dd294f76d1b291cbf8afce463c8
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-05 18:35:56 -0500

    Delete newTab.js

--- newTab.js
+++
@@ -1,5 +0,0 @@
-var bingUrl = "https://www.bing.com/chrome/newtab?origin=ext&pc=U470";
-function setNewTabPage(online) {
-    var newTabFrame = document.getElementById("ntp-frame");
-    newTabFrame.src = bingUrl;
-}
